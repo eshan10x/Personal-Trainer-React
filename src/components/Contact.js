a9568f94886ccbc48c70ff8cfd79c2cc72a91bca
Eshan <eshan@Eshans-MacBook-Air.local>
2022-07-02 18:06:02 +0530

    updated bug

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,7 +17,7 @@ function Contact() {
             </div>
 
             <div className="rowContact">
-                <div className="col-2">
+                <div className="fol-2 col1">
                     <div className='form'>
                     <Box sx={{ width: '100%' }}>
                         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: -15, md: 0 }}>
@@ -77,7 +77,7 @@ function Contact() {
                     </Box>
                     </div>
                 </div>
-                <div className="col-1">
+                <div className="col-2 col2">
                     <h1>shdhdhdhh</h1>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3258.224049167725!2d-80.79523568479603!3d35.25068148029693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8856a074bfa0fdeb%3A0x2e0f09a3c787d5ae!2sThe%20Fitness%20Factory!5e0!3m2!1sen!2s!4v1609492245998!5m2!1sen!2s" width="500" height="350" frameborder="0" style={{ border: "0" }} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
