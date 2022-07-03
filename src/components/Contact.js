10beb31027f85bacf153850a7c1f7e35c9634521
Eshan <eshan@Eshans-MacBook-Air.local>
2022-07-04 00:08:49 +0530

    fixed home page align

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,11 +13,14 @@ function Contact() {
         <div className="contactPage">
             <div className="contact__header">
                 <Navbar />
-                <h1>Contact Us</h1>
+                <h1 id='subhead1'>Contact Us</h1>
+                <p id='subhead'>FEEL FREE TO GET IN TOUCH TO FIND OUT HOW WE {'\n'} 
+                CAN HELP YOU SOLVE YOUR STRENGTH OR FITNESS GOALS.</p>
             </div>
 
             <div className="rowContact">
                 <div className="col-2">
+                    <h1 id='contactusheader'>Contact Us</h1>
                     <div className='form'>
                         <Box sx={{ width: '100%' }}>
                             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: -15, md: 0 }}>
