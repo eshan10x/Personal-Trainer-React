cb97f6fc367d012429e0de67f62463866b09d7dd
Eshan <eshan@Eshans-MacBook-Air.local>
2022-07-17 18:03:59 +0530

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import '../css/Footer.css';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import PhoneIcon from '@material-ui/icons/Phone';
-import MailIcon from '@material-ui/icons/Mail';
+import React from "react";
+import "../css/Footer.css";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <div className="contact">
-                <h3>Contact Us!</h3>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm">
-                            <div className="street">
-                                <InstagramIcon fontSize="large" />
-                            </div>
-                        </div>
-                        <div class="col-sm">
-                            <div className="phone">
-                                <PhoneIcon />
-                            </div>
-                        </div>
-                        <div class="col-sm">
-                            <div className="mail">
-                                <MailIcon />
-                                {/* <p>fitnesswithnaveen@gmail.com</p> */}
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div className="footer">
+      <div className="contact">
+        <h3>Contact Us</h3>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <a href="https://instagram.com/fitness_with_naveen?igshid=YmMyMTA2M2Y=">
+                <div className="street">
+                  <InstagramIcon fontSize="large" />
                 </div>
-
-
+              </a>
             </div>
-            <div className="copyright">
-                <p>© Copyright Fitness With Naveen. All Right Reserved.</p>
+            <div class="col-sm">
+              <div className="phone">
+                <FacebookIcon fontSize="large" />
+              </div>
             </div>
+            <div class="col-sm">
+              <a href="tel:+94766876602">
+                <div className="mail">
+                  <PhoneIcon fontSize="large" />
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      <div className="copyright">
+        <p>© Copyright Fitness With Naveen. All Right Reserved.</p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
